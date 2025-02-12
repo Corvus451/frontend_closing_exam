397482e80b7c4b64a276fcae78117d55b74eede0
Corvus <msmark.451@gmail.com>
2025-02-12 15:04:54 +0100

    task 1 completed and working

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,25 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
 
+    const actor = allActors.find((actor)=> actor.name == actorName);
+    // console.log(actor);
+    
+    if(!actor) {
+        return [];
+    }
+
+    // const found = allMovies.reduce((prev, current)=> {
+    //     console.log("prev:", prev);
+    //     return prev;
+    // }, [])
+
+    return allMovies.reduce((result, current)=> {
+        if(current.actor_ids.includes(actor.id) && current.genres.includes(genre)) {
+
+            result = [...result, current.title];
+            console.log("result: ", result);
+        }
+        return result;
+    }, []);
+    
+
 }
