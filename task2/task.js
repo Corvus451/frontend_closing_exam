4ccf054a61fd54f7297704cc25d73333dcc2b5cd
Corvus <msmark.451@gmail.com>
2025-02-12 15:05:13 +0100

    task 2 completed and working

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,22 @@
 export function findUniqueGenres(genres) {
 
+    const uniqueGenres = [];
+
+    for(let i=0; i<genres.length; i++) {
+
+        const movie = genres[i];
+
+        for(let j=0; j< movie.length; j++) {
+
+            const currentGenre = movie[j];
+
+            if(!uniqueGenres.includes(currentGenre)) {
+
+                uniqueGenres.push(currentGenre);
+            }
+        }
+    }
+
+    return uniqueGenres;
+
 }
